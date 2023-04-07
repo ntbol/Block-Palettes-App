1f993367affdb5f1b9653a31ad63db689684179f
Nate Boland <ntboland@gmail.com>
2023-04-07 15:20:06 -0400

    Initial Commit

---
+++ Block Palettes/Block_PalettesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_PalettesApp.swift
+//  Block Palettes
+//
+//  Created by Nathan Boland on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Block_PalettesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
